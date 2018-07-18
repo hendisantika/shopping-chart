5fe9e70970faccb2aad2294636c12b29251efe89
Hendi Santika <hendisantika@gmail.com>
2018-07-19 05:26:03 +0700

    #6 - Modify Main App

--- src/main/java/com/hendisantika/shoppingchart/ShoppingChartApplication.java
+++ src/main/java/com/hendisantika/shoppingchart/ShoppingChartApplication.java
@@ -1,10 +1,23 @@
 package com.hendisantika.shoppingchart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { //
+        DataSourceAutoConfiguration.class, //
+        DataSourceTransactionManagerAutoConfiguration.class, //
+        HibernateJpaAutoConfiguration.class})
+
 public class ShoppingChartApplication {
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingChartApplication.class, args);
