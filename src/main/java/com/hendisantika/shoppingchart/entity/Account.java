17ff5fa5937ced8b6750fa01855389505a440af5
Hendi Santika <hendisantika@gmail.com>
2018-07-20 06:47:09 +0700

    #9 - Add Account Entity

---
+++ src/main/java/com/hendisantika/shoppingchart/entity/Account.java
@@ -0,0 +1,76 @@
+package com.hendisantika.shoppingchart.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : shopping-chart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/07/18
+ * Time: 06.46
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "Accounts")
+public class Account implements Serializable {
+
+    public static final String ROLE_MANAGER = "MANAGER";
+    public static final String ROLE_EMPLOYEE = "EMPLOYEE";
+    private static final long serialVersionUID = -2054386655979281969L;
+    @Id
+    @Column(name = "User_Name", length = 20, nullable = false)
+    private String userName;
+
+    @Column(name = "Encryted_Password", length = 128, nullable = false)
+    private String encrytedPassword;
+
+    @Column(name = "Active", length = 1, nullable = false)
+    private boolean active;
+
+    @Column(name = "User_Role", length = 20, nullable = false)
+    private String userRole;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEncrytedPassword() {
+        return encrytedPassword;
+    }
+
+    public void setEncrytedPassword(String encrytedPassword) {
+        this.encrytedPassword = encrytedPassword;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.userName + "," + this.encrytedPassword + "," + this.userRole + "]";
+    }
+
+}
