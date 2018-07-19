cdbde0eda04e3a1d8a28c238a8c65033e1f525b8
Hendi Santika <hendisantika@gmail.com>
2018-07-20 06:52:09 +0700

    #14 - Add Account DAO

---
+++ src/main/java/com/hendisantika/shoppingchart/dao/AccountDAO.java
@@ -0,0 +1,32 @@
+package com.hendisantika.shoppingchart.dao;
+
+import com.hendisantika.shoppingchart.entity.Account;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : shopping-chart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/07/18
+ * Time: 06.51
+ * To change this template use File | Settings | File Templates.
+ */
+@Transactional
+@Repository
+public class AccountDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public Account findAccount(String userName) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.find(Account.class, userName);
+    }
+
+}
