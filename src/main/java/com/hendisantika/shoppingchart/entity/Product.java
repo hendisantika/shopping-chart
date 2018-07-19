a26b235890ff859d9c845d48a4f3c7bd973851c5
Hendi Santika <hendisantika@gmail.com>
2018-07-20 06:48:17 +0700

    #10 - Add Product Entity

---
+++ src/main/java/com/hendisantika/shoppingchart/entity/Product.java
@@ -0,0 +1,90 @@
+package com.hendisantika.shoppingchart.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : shopping-chart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/07/18
+ * Time: 06.47
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "Products")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = -1000119078147252957L;
+
+    @Id
+    @Column(name = "Code", length = 20, nullable = false)
+    private String code;
+
+    @Column(name = "Name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name = "Price", nullable = false)
+    private double price;
+
+    @Lob
+    @Column(name = "Image", length = Integer.MAX_VALUE, nullable = true)
+    private byte[] image;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "Create_Date", nullable = false)
+    private Date createDate;
+
+    public Product() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+}
