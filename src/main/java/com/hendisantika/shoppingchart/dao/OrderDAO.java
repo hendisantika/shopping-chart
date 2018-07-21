4337268e11d7ead6789637c3a6164bdd5dd1ffcb
Hendi Santika <hendisantika@gmail.com>
2018-07-22 06:03:13 +0700

    #30 - Add Product DAO

--- src/main/java/com/hendisantika/shoppingchart/dao/OrderDAO.java
+++ src/main/java/com/hendisantika/shoppingchart/dao/OrderDAO.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
