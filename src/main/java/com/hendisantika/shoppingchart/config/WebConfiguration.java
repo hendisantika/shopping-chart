14a9549f251453444f5bbc7243a3e695ab5a7308
Hendi Santika <hendisantika@gmail.com>
2018-07-22 06:00:56 +0700

    #27 - Add Web Configuration

---
+++ src/main/java/com/hendisantika/shoppingchart/config/WebConfiguration.java
@@ -0,0 +1,31 @@
+package com.hendisantika.shoppingchart.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : shopping-chart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/07/18
+ * Time: 06.00
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        // Load file: validation.properties
+        messageSource.setBasename("classpath:validation");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
