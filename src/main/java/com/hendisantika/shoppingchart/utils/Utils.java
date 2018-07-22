99d29c155934760925c92d588a6ab726f0b9d72a
Hendi Santika <hendisantika@gmail.com>
2018-07-23 04:26:40 +0700

    #31 - Add Utils Class

---
+++ src/main/java/com/hendisantika/shoppingchart/utils/Utils.java
@@ -0,0 +1,44 @@
+package com.hendisantika.shoppingchart.utils;
+
+import com.hendisantika.shoppingchart.model.CartInfo;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : shopping-chart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/07/18
+ * Time: 04.26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Utils {
+    // Products in the cart, stored in Session.
+    public static CartInfo getCartInSession(HttpServletRequest request) {
+
+        CartInfo cartInfo = (CartInfo) request.getSession().getAttribute("myCart");
+
+
+        if (cartInfo == null) {
+            cartInfo = new CartInfo();
+
+            request.getSession().setAttribute("myCart", cartInfo);
+        }
+
+        return cartInfo;
+    }
+
+    public static void removeCartInSession(HttpServletRequest request) {
+        request.getSession().removeAttribute("myCart");
+    }
+
+    public static void storeLastOrderedCartInSession(HttpServletRequest request, CartInfo cartInfo) {
+        request.getSession().setAttribute("lastOrderedCart", cartInfo);
+    }
+
+    public static CartInfo getLastOrderedCartInSession(HttpServletRequest request) {
+        return (CartInfo) request.getSession().getAttribute("lastOrderedCart");
+    }
+}
